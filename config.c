e0f0780d59073fdc11c4789b5459774f278604c0
Szymon Chwastek <chwastekszymon@gmail.com>
2025-01-20 00:33:24 +0100

    feat: config file

---
+++ config.c
@@ -0,0 +1,5 @@
+//
+// Created by Szymon on 1/19/2025.
+//
+
+#include "config.h"
---
+++ config.h
@@ -0,0 +1,11 @@
+//
+// Created by Szymon on 1/19/2025.
+//
+
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#define SEM_TRAIN_OPEN 2
+#define SEM_TRAIN_CLOSE 2
+
+#endif //CONFIG_H
