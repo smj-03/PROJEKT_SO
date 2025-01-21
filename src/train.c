afbcdc79451520d3a24e0bd6a306a76743a95beb
Szymon Chwastek <chwastekszymon@gmail.com>
2025-01-21 12:20:10 +0100

    refactor: one open doors function

--- src/train.c
+++ src/train.c
@@ -13,20 +13,27 @@ struct train {
     int passenger_count;
     int bike_count;
 
-    int sem_id_td_p;
-    int sem_id_td_c;
+    // int sem_id_td_p;
+    // int sem_id_td_c;
+    // pthread_mutex_t* mutex;
 };
 
-void init_train(struct train *, int, int);
+struct thread_args {
+    int door_number;
+    int sem_id_td_p;
+    int sem_id_td_c;
+    pthread_mutex_t *mutex;
+    struct train *this;
+};
 
-void *open_doors_1(void *);
-
-void *open_doors_2(void *);
+void *open_doors(void *);
 
 void exit_(const char *);
 
 int main(int argc, char *argv[]) {
-    const int sem_id_td_p = sem_alloc(SEM_T_DOOR_P, SEM_T_DOOR_NUM, IPC_GET);
+    pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+
+    const int sem_id_td_p = sem_alloc(SEM_T_DOOR_P_KEY, SEM_T_DOOR_NUM, IPC_GET);
     if (sem_id_td_p == IPC_ERROR) exit_("Semaphore Allocation Error");
 
     const int sem_id_td_c = sem_alloc(SEM_T_DOOR_C, SEM_T_DOOR_NUM, IPC_GET);
@@ -36,6 +43,26 @@ int main(int argc, char *argv[]) {
     init_train(this, sem_id_td_p, sem_id_td_c);
 
     if (this == NULL) exit_("Train Creation");
+    this->id = getpid();
+    this->passenger_count = 0;
+    this->bike_count = 0;
+
+    struct thread_args *args_1 = malloc(sizeof(struct thread_args));
+    if (args_1 == NULL) exit_("Thread Arguments Creation");
+    args_1->door_number = 0;
+    args_1->sem_id_td_p = sem_id_td_p,
+            args_1->sem_id_td_c = sem_id_td_c;
+    args_1->mutex = &mutex;
+    args_1->this = this;
+
+    struct thread_args *args_2 = malloc(sizeof(struct thread_args));
+    if (args_2 == NULL) exit_("Thread Arguments Creation");
+    args_2->door_number = 1;
+    args_2->sem_id_td_p = sem_id_td_p,
+            args_2->sem_id_td_c = sem_id_td_c;
+    args_2->mutex = &mutex;
+    args_2->this = this;
+
 
     // log_message(
     //     PROCESS_NAME,
@@ -47,8 +74,8 @@ int main(int argc, char *argv[]) {
     // 1 - baggage 2 - bike
     pthread_t id_thread_door_1, id_thread_door_2;
 
-    if (pthread_create(&id_thread_door_1, NULL, open_doors_1, this)) exit_("Thread 1 Creation");
-    if (pthread_create(&id_thread_door_2, NULL, open_doors_2, this)) exit_("Thread 2 Creation");
+    if (pthread_create(&id_thread_door_1, NULL, open_doors, args_1)) exit_("Thread 1 Creation");
+    if (pthread_create(&id_thread_door_2, NULL, open_doors, args_2)) exit_("Thread 2 Creation");
 
     if (pthread_join(id_thread_door_1, NULL)) exit_("Thread 1 Join");
     if (pthread_join(id_thread_door_2, NULL)) exit_("Thread 2 Join");
@@ -57,49 +84,27 @@ int main(int argc, char *argv[]) {
     if (!pthread_detach(id_thread_door_2)) exit_("Thread 2 Detach");
 
     free(this);
+    free(args_1);
+    free(args_2);
+    shared_block_destroy(SHM_TRAIN_DOOR_1_KEY);
+    shared_block_destroy(SHM_TRAIN_DOOR_2_KEY);
 }
 
-void init_train(struct train *this, int sem_id_td_p, int sem_id_td_c) {
-    this->id = getpid();
-    this->passenger_count = 0;
-    this->bike_count = 0;
-    this->sem_id_td_p = sem_id_td_p,
-    this->sem_id_td_c = sem_id_td_c;
-}
+void *open_doors(void *_args) {
+    struct thread_args *args = _args;
+    struct train *this = args->this;
+    log_message(PROCESS_NAME, "[THREAD] Doors %d\n", args->door_number + 1);
 
-void *open_doors_1(void *_this) {
-    log_message(PROCESS_NAME, "[THREAD] Doors 1\n");
-    struct train *this = _this;
     while (1) {
-        const int post_res = sem_post(this->sem_id_td_p, 0);
+        const int post_res = sem_post(args->sem_id_td_p, 0);
         if (post_res == IPC_ERROR) exit_("Semaphore Init Post");
 
-        sem_wait(this->sem_id_td_c, 0, 0);
+        sem_wait(args->sem_id_td_c, args->door_number, 0);
 
-        pthread_mutex_lock(&mutex);
+        pthread_mutex_lock(args->mutex);
         this->passenger_count++;
         log_message(PROCESS_NAME, "[BOARDING] Passenger has entered. Current number: %d\n", this->passenger_count);
-        pthread_mutex_unlock(&mutex);
-    }
-}
-
-void *open_doors_2(void *_this) {
-    log_message(PROCESS_NAME, "[THREAD] Doors 2\n");
-    struct train *this = _this;
-    while (1) {
-        const int post_res = sem_post(this->sem_id_td_p, 1);
-        if (post_res == IPC_ERROR) exit_("Semaphore Init Post");
-
-        sem_wait(this->sem_id_td_c, 1, 0);
-
-        pthread_mutex_lock(&mutex);
-        this->passenger_count++;
-        this->bike_count++;
-        log_message(PROCESS_NAME,
-            "[BOARDING] Passenger with a bike has entered. Current number: %d Bike number:%d\n",
-            this->passenger_count,
-            this->bike_count);
-        pthread_mutex_unlock(&mutex);
+        pthread_mutex_unlock(args->mutex);
     }
 }
 
